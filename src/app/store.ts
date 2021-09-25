a5795c546193331626b03aa4a99388fc4b834d75
Jutr0 <micwikski@gmail.com>
2021-09-25 20:54:58 +0200

    desktopDesign done

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,7 +1,8 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import reducer from './reducer';
 
 export const store = configureStore({
-	reducer: {},
+	reducer,
 });
 
 export type AppDispatch = typeof store.dispatch;
